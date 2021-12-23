431c4216b384cf48376d4f1751c3838a9e332633
iMaary <m.rodriguesnave@gmail.com>
2021-12-23 10:15:11 -0300

    creating a model

---
+++ app/routes/application.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+// import Ember from 'ember';
+// export default Ember.Route.extend({
+    
+// });
+
+export default class ApplicationRoute extends Route {
+
+    model(){
+        return 'Eu sla vei'
+    }
+}
