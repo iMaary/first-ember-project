f945f1cf08401fec42e934217262458249760043
iMaary <m.rodriguesnave@gmail.com>
2021-12-25 16:00:20 -0300

    interactive rental/image

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,3 +1,19 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class RentalImageComponent extends Component {}
+export default class RentalImageComponent extends Component {
+  //first implements
+  // constructor(...args) {
+  //   super(...args);
+  //   this.isLarge = false;
+  // }
+  
+  //second mode implements
+  // isLarge = false;
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+  }
+}
