f7cf5d25d75e7bfa454b6f0c4ecde90769d505c7
iMaary <m.rodriguesnave@gmail.com>
2021-12-25 13:30:34 -0300

    testing about and contact templates

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -17,11 +17,37 @@ module('Acceptance | super rentals', function (hooks) {
     assert.equal(currentURL(), '/');
     // Searching for the text in h2 tag
     assert.dom('h2').hasText('Welcome to Super Rentals!');
+
     // and searching for the text in button from jumbo class
     assert.dom('.jumbo a.button').hasText('About Us');
     // Accessing the button URL 
     await click('.jumbo a.button');
+
     // and checking if the current URL is being for about route
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+
+    assert.equal(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting getting-in-touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch')
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About');
+    await click('.jumbo a.button');
+
     assert.equal(currentURL(), '/about');
   });
 });
