2f3ed3d9d73bd6be0b62c001c6a1f29e6055195f
iMaary <m.rodriguesnave@gmail.com>
2021-12-27 10:46:40 -0300

    some adjusts to fix not found environment path

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import ENV from '/config/environment';
+import ENV from '../../../config/environment';
 
 module('Integration | Component | map', function (hooks) {
   setupRenderingTest(hooks);
