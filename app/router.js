e6887d73c46df571be085f4ab5b4547a1c0b5391
iMaary <m.rodriguesnave@gmail.com>
2022-01-23 16:43:05 -0300

    using routes

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('/');
+  this.route('papers', { path: '/about/' });
+  this.route('staplers', { path: '/my_staplers/' });
+  this.route('page-not-found', { path: '/*wildcard' });
+});
